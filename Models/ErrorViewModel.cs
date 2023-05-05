546539715d8afb361ec289340cab954e54979ce9
dumitreskw <ovidumitreskw@gmail.com>
2023-05-05 19:10:11 +0300

    add initial solution

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BusinessTrips.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
